aa962a5ada5161b2fdf3ea2ed64e69fd184c985a
Rodriguez Rasero <santiago.rodriguez@atos.net>
2020-03-16 13:18:10 +0100

    test

--- src/main/java/com/codechallenge/SearchForm.java
+++ src/main/java/com/codechallenge/SearchForm.java
@@ -1,13 +1,17 @@
 package com.codechallenge;
 
+/*
+ * Class to receive parameters from the call
+ */
 public class SearchForm {
+	
 	String iban;
 	String sort;
 	public SearchForm(String iban, String sort) {
 		super();
 		this.iban = iban;
 		this.sort = sort;
-	}
+	} 
 	public String getIban() {
 		return iban;
 	}
